d712b2b4f17faab3d915958e79f289ab0e0b189e
sherrrr468 <lakshay.pvt3@gmail.com>
2024-07-26 11:45:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7374733018:AAHu5LZrQcfbpGidkD9FzJnVYbziG9JBDX0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("Lakshay_03z")
+OWNER_USERNAME=("Lakshayz")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6984603270")
